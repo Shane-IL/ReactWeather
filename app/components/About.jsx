e56e2de1dc7a123f8c33d5f2b3ed8508bd0d8c83
Shane Goodson <shane@webzai.com>
2017-01-28 21:18:48 +0200

    Renamed About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const About = (props) =>(<h3>About Component</h3>);
+const About = (props) =>(
+    <div>
+        <h3>About</h3>
+        <p>Welcome to the About page.</p>
+    </div>
+);
 
 export {About as default};
