89b9c93fc8707b6f987b78c6f6bdcb35b1f3eb4e
Shane Goodson <shane@webzai.com>
2017-01-28 17:49:27 +0200

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Examples = (props) => (<h3>Examples Component</h3>);
+const Examples = (props) => (
+    <div>
+        <h3>Examples Component</h3>
+        <p>Welcome to the examples file!</p>
+    </div>
+);
 
 export {Examples as default};
