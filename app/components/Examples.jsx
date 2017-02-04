de32e3ba560d394057d25aaa0a4539aaa76a77f9
Shane Goodson <shane@webzai.com>
2017-02-04 23:23:08 +0200

    Styled examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const Examples = (props) => (
     <div>
-        <h3>Examples</h3>
-        <p>Welcome to the examples file!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out</p>
+        <ol>
+            <li>
+                <Link to='/?location=Tel%20Aviv'>Tel Aviv, Israel</Link>
+            </li>
+            <li>
+                <Link to='/?location=Pretoria'>Pretoria, South Africa</Link>
+            </li>
+        </ol>
     </div>
 );
 
