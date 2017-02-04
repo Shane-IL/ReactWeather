c857911fd60b5fd61f913a9793d8a77dc07f74ec
Shane Goodson <shane@webzai.com>
2017-02-04 23:30:41 +0200

    Styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const About = (props) =>(
     <div>
-        <h3>About</h3>
-        <p>Welcome to the About page.</p>
+        <h1 className="text-center">About</h1>
+        <p>
+            This is a weather application built with React. I made this for the Complete React Web App Developer Course.
+        </p>
+        <p>
+            Here are some of the tools I used:
+        </p>
+        <ul>
+            <li>
+                <a href="http://facebook.github.io/react">React</a> - This is the React Javascript framework, used to create this app.
+            </li>
+            <li>
+                <a href="http://openweathermap.org">Open Weather Map</a> - This is the weather service API used to provide weather data for the app.
+            </li>
+        </ul>
     </div>
 );
 
